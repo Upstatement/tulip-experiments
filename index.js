3c487b164f8be488da46e3d3780ee63f5d024b4c
Alec Marcus <alec@alecmarc.us>
2021-03-04 14:55:16 -0500

    add form

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 import Paper from "paper";
 
-const abc = "abcdefghijklmnopqrstuvwxyz";
-
-const appNames = [
-  "Audit Event Form",
-  "Centralized Time Tracking",
-  "Digital Andon Monitoring",
-  "Digital Andon System",
-  "Digital Logbook",
-  "Digital Logbook with Work Instructions"
-];
-
 const ArtGenerator = {
-  getAppName(i) {
-    return appNames[i].toLowerCase().split(" ");
+  appNames: [
+    "Audit Event Form",
+    "Centralized Time Tracking",
+    "Digital Andon Monitoring",
+    "Digital Andon System",
+    "Digital Logbook",
+    "Digital Logbook with Work Instructions"
+  ],
+
+  get appNameSelectEL() {
+    return document.getElementById("appNameSelect");
+  },
+  get customPhraseInputEl() {
+    return document.getElementById("customPhraseInput");
   },
 
-  getDegreeOf(letter) {
-    return 360 / 26 * (1 + abc.indexOf(letter));
+  populateAppNameSelectOptions() {
+    this.appNames.forEach((name, i) => {
+      const optionEl = document.createElement("option");
+      optionEl.value = name;
+      optionEl.selected = i === 0;
+      optionEl.textContent = name;
+      this.appNameSelectEL.appendChild(optionEl);
+    })
   },
 
-  r: 100,
+  domSetup() {
+    this.populateAppNameSelectOptions();
+    this.appNameSelectEL.addEventListener("change", (e) => this.updateArt(e.target.value))
+    this.customPhraseInputEl.addEventListener("change", (e) => this.updateArt(e.target.value))
+  },
+
+  r: 150,
 
   drawAsLine(word) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const getDegreeOf = letter => 360 / 26 * (1 + alphabet.indexOf(letter));
+
     const path = new Paper.Path();
 
     [...word].forEach(letter => {
-      const theta = this.getDegreeOf(letter);
+      const theta = getDegreeOf(letter);
       const x = this.r * Math.sin(theta) + Paper.view.center.x;
       const y = this.r * Math.cos(theta) + Paper.view.center.y;
       path.add(new Paper.Point(x, y));
@@ -35,28 +51,37 @@ const ArtGenerator = {
     return path;
   },
 
-  generateArt() {
+  onResize: null,
+  generateArt(phraseToDraw) {
     const circle = new Paper.Path.Circle(Paper.view.center, this.r);
 
-    const wordPaths = this.getAppName(3).map(word => this.drawAsLine(word));
+    const sanitizedPhrase = phraseToDraw.toLowerCase().split(" ");
+    const wordPaths = sanitizedPhrase.map(word => this.drawAsLine(word));
 
     const group = new Paper.Group([circle, ...wordPaths]);
     group.strokeColor = "black";
 
-    window.addEventListener("resize", () => {
-      group.position = Paper.view.center;
-    });
+    Paper.view.draw();
+
+    this.onResize = () => group.position = Paper.view.center;
+    window.addEventListener("resize", this.onResize);
+  },
+  updateArt(phraseToDraw) {
+    window.removeEventListener("resize", this.onResize);
+    Paper.project.clear();
+    this.generateArt(phraseToDraw);
   },
 
-  init () {
-    const canvas = document.getElementById('canvas');
+  init() {
+    this.domSetup();
 
+    const canvas = document.getElementById('canvas');
     Paper.setup(canvas);
 
-    this.generateArt();
-
-    Paper.view.draw();
+    this.generateArt(this.appNameSelectEL.value);
   }
 };
 
-document.addEventListener("DOMContentLoaded", ArtGenerator.init());
+document.addEventListener("DOMContentLoaded", () => {
+  ArtGenerator.init();
+});
